e89c62e46cd46b83aa45357f6f778377cdd6f1ce
Joe Cserko <j.cserko@gmail.com>
2021-05-05 12:08:56 +0200

    Feat: mongoose.

--- express/app.js
+++ express/app.js
@@ -6,6 +6,8 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var rfs = require('rotating-file-stream') // version 2.x
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +21,16 @@ var accessLogStream = rfs.createStream('access.log', {
   path: path.join(__dirname, 'log')
 });
 
+// Connect with Mongoose.
+( async () => {
+  await mongoose.connect('mongodb://127.0.0.1:27017/users', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+})();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -28,6 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( cors() );
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- express/routes/api.js
+++ express/routes/api.js
@@ -2,31 +2,67 @@ var express = require('express');
 var router = express.Router();
 const { readFile } = require('fs').promises;
 const { join } = require('path');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const UserSchema = new Schema({
+  name: String,
+  email: String,
+  created: Date,
+  address: String
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   next();
 });
 
-router.get('/users', async (req, res, next) => {
-    const usersContent = await readFile( join(__dirname, '../db/users.json'), 'utf8' );
-    res.json( JSON.parse(usersContent) );
+router.post('/users', async (req, res, next) => {
+    const instance = new UserModel({
+        name: 'Arnold',
+        email: 'schwarczi@gmail.com',
+        created: new Date(),
+        address: 'Los Angeles'
+    });
+    instance.save( (err) => {
+        if (err) {
+            return res.json(err);
+        }
+        res.json({success: true});
+    });
 });
 
-router.get('/users/:id', async (req, res, next) => {
-    const usersContent = await readFile( join(__dirname, '../db/users.json'), 'utf8' );
-    const users = JSON.parse(usersContent);
-    const id = Number(req.params.id);
-    if (!id) {
-        return next();
-    }
-    const user = users.filter( u => u.id === id )[0];
 
-    if (!user) {
-        return next();
-    }
 
-    res.json( user );
-});
+
+
+
+
+// Json based database.
+// router.get('/users', async (req, res, next) => {
+//     const usersContent = await readFile( join(__dirname, '../db/users.json'), 'utf8' );
+//     res.json( JSON.parse(usersContent) );
+// });
+
+// router.get('/users/:id', async (req, res, next) => {
+//     const usersContent = await readFile( join(__dirname, '../db/users.json'), 'utf8' );
+//     const users = JSON.parse(usersContent);
+//     const id = Number(req.params.id);
+//     if (!id) {
+//         return next();
+//     }
+//     const user = users.filter( u => u.id === id )[0];
+
+//     if (!user) {
+//         return next();
+//     }
+
+//     res.json( user );
+// });
 
 module.exports = router;
